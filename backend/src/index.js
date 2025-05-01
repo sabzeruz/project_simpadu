13167a24ac0b799d1e33874b222a834112703376
luputer <m.saidibjm4@gmail.com>
2025-05-01 10:55:40 +0800

    add auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+// 
 const pegawaiRoutes = require('./routes/pegawaiRoutes'); //path pegawaiRoutes.js
+const authRoutes = require('./routes/authRoutes'); //path authRoutes.js 
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors());
 
 const swaggerDefinition = {
   openapi: '3.0.0',
@@ -15,17 +24,15 @@ const swaggerDefinition = {
     version: '1.0.0',
     description: 'Dokumentasi API Simpadu untuk 2 level user',
   },
-  servers: [
-    {
-      url: 'http://localhost:3000',
-    },
-  ],
+  servers: [{
+    url: 'http://localhost:3000',
+  }, ],
 };
 
 //swagger-jsdoc
 const options = {
   swaggerDefinition,
-  apis: ['./src/routes/pegawaiRoutes.js'], 
+  apis: ['./src/routes/pegawaiRoutes.js'],
 };
 
 // Inisialisasi swagger-jsdoc
@@ -36,9 +43,13 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Menyambungkan rute API
 app.use('/api', pegawaiRoutes);
+app.use('/auth', authRoutes);
 
 // Memulai server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
 });
